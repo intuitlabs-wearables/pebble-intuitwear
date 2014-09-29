32be1e6126a6e53c3348b8bc86e0b3d0567e012f
Maidasani <hmaidasani@MTVL121d69b22.local>
2014-09-29 12:07:18 -0700

    Initial Commit

---
+++ IntuitWear/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntuitWear
+//
+//  Created by Maidasani, Hitesh on 9/29/14.
+//  Copyright (c) 2014 intuit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
