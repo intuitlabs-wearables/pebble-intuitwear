925f6745659826b253c45fd881147bab32b1a842
Maidasani <hmaidasani@MTVL121d69aec.local>
2015-01-26 14:36:40 -0800

    Adding Copyright and MIT License

--- IntuitWear/DashboardViewController.m
+++ IntuitWear/DashboardViewController.m
@@ -1,10 +1,34 @@
 //
-//  DashboardViewController.m
-//  IntuitWear
+// DashboardViewController.m
+// IntuitWear
 //
-//  Created by Maidasani, Hitesh on 10/10/14.
-//  Copyright (c) 2014 intuit. All rights reserved.
+// Created by Maidasani, Hitesh on 01/26/15.
 //
+// Copyright (c) 2015 Intuit Inc. All rights reserved.
+// Unauthorized reproduction is a violation of applicable law.
+// This material contains certain confidential and proprietary information and trade secrets of Intuit Inc.
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Intuit Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "DashboardViewController.h"
 #import "KeychainItemWrapper.h"
